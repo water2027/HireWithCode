3b021c17739587de89973b9cdb4baa677f8ce784
water <linshy76@mail2.sysu.edu.cn>
2025-05-27 01:40:24 +0800

    修复路径问题

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,7 +6,7 @@ import UnoCSS from 'unocss/vite'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
-const base = process.env.VITE_BASE_URL || '/'
+const base = '/hire'
 export default defineConfig({
   base,
   plugins: [vue(), vueJsx(), UnoCSS()],
